ac047dbc5d348e9c944c53be25e0b17157d03cb1
acer <newlinefu@gmail.com>
2022-08-15 22:22:47 +0300

    Init commit

---
+++ src/main/java/com/example/roadmapdatasaver/RoadmapDataSaverApplication.java
@@ -0,0 +1,11 @@
+package com.example.roadmapdatasaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapDataSaverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoadmapDataSaverApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/roadmapdatasaver/RoadmapDataSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roadmapdatasaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapDataSaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
